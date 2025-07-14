58c4c6ca5a49626eb17537882f8f5c00316f4d5e
kirubhakbr17 <kirubhakbr17@gmail.com>
2025-07-13 20:00:24 -0930

    updated animation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot} from 'react-dom/client'
-import App from './App.jsx'
-import Car from './App.jsx'
 
 
-import Router from './assets/projects/Router.jsx'
+
+
+
 import Portfolio from './assets/projects/portfolio.jsx'
 
 
